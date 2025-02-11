7f32108c2a62a0c0a99dc68b61c5c63a63affa77
logan579 <iradukundafiette10@gmail.com>
2025-02-11 17:03:10 +0200

    caluculater app

---
+++ Desktop/vie-first-project/logan579/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Welcome</h1>
+  <HtmlCalculator />
+</template>
+
+<script>
+import HtmlCalculator from './components/HtmlCalculater.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HtmlCalculator,
+  },
+};
+</script>
+

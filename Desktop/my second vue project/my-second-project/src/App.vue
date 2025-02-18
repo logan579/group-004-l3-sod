a101d2a96819416e17f5d01b22e7a48787274e4e
logan579 <iradukundafiette10@gmail.com>
2025-02-18 16:44:15 +0200

    add:tested web page

---
+++ Desktop/my second vue project/my-second-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WelcomePage />
+</template>
+
+<script>
+import WelcomePage from './components/WelcomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WelcomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Desktop/my second vue project/my-second-project/src/components/WelcomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>welcome</h1>
+    <h2>stay tune</h2>
+    <h2>move on</h2>
+</template>
+<script>
+export default {
+name: "welcomePage",
+}
+</script>
